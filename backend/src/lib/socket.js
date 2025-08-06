be25189d5e744ea1087220bcdcb51f9135ce7395
Danyal538 <danyalatique0@gmail.com>
2025-08-06 20:40:25 +0500

    deployment activites

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -14,7 +14,7 @@ const userSocketMap = {}; // {userId: socketId}
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"]
+        origin: ["https://chat-app-sigma-azure.vercel.app"]
     }
 })
 
